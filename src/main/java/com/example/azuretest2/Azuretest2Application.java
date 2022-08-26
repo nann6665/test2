10fb609c14a14a2f50bc3c13bd363dfa69698490
nann6665 <nann6665@kea.stud.dk>
2022-08-26 09:29:29 +0200

    test2

---
+++ src/main/java/com/example/azuretest2/Azuretest2Application.java
@@ -0,0 +1,13 @@
+package com.example.azuretest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azuretest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Azuretest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azuretest2/Azuretest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azuretest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Azuretest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
